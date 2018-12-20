8ca5225df39563cc2543bb3f96ab29fc41777709
Acer79 <lsg.tech79@gmail.com>
2018-12-20 23:10:47 +0000

    First Commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+export const appconfig = {
+    
+  firebase: {
+    apiKey: "AIzaSyCp9S97OF_myFVKLNwMb095OAWcx5HgK0Q",
+    authDomain: "chatapp-854f1.firebaseapp.com",
+    databaseURL: "https://chatapp-854f1.firebaseio.com",
+    projectId: "chatapp-854f1",
+    storageBucket: "chatapp-854f1.appspot.com",
+    messagingSenderId: "588266487240"
+  },
+  users_endpoint: "users",
+  chats_endpoint: "chats"
+};
+
+    
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,45 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { appconfig } from "./app.config";
+import { ChatService } from "./app.service";
+import { ChatsPage } from "../pages/chats/chats";
+import { ChatroomPage } from "../pages/chatroom/chatroom";
+import { SortPipe } from "../pipes/sort/sort";
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatsPage,
+    ChatroomPage,
+    SortPipe
   ],
   imports: [
+    AngularFireModule.initializeApp(appconfig.firebase),
+    AngularFirestoreModule,
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatsPage,
+    ChatroomPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ChatService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
